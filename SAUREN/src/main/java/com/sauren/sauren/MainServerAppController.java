c1d2ac00b5bc59c4e75d046fcdb3be277c619f9f
ilyasidor <deadboost.666@gmail.com>
2023-05-13 17:14:49 +0900

    Добавлена проверка портов.
                checkPort(  Integer.parseInt(   serverPortTF.getText()  ) );

--- SAUREN/src/main/java/com/sauren/sauren/MainServerAppController.java
+++ SAUREN/src/main/java/com/sauren/sauren/MainServerAppController.java
@@ -7,10 +7,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
-import java.net.Inet4Address;
-import java.net.URL;
-import java.net.UnknownHostException;
+import java.io.IOException;
+import java.net.*;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
@@ -18,21 +18,43 @@ public class MainServerAppController implements Initializable
 {
     @FXML
     public ChoiceBox choiceUserStatusCB;
+
+    @FXML
+    public TextField serverPortTF;//порт
     @FXML
     private Label serverIPLbl;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle)
     {
         try {
+            //проверяем, занят ли порт и устанавливаем свободный если он занят!
+            checkPort(  Integer.parseInt(   serverPortTF.getText()  ) );
+
+
             serverIPLbl.setText(Inet4Address.getLocalHost().getHostAddress());
         } catch (UnknownHostException e) {
             throw new RuntimeException(e);
         }
-
         //Заполнение ChoiceBox значениями о статусе пользователя.
         ObservableList<String> userStatus = FXCollections.observableArrayList("Online", "Offline", "All Users");
         choiceUserStatusCB.setItems(userStatus);
         choiceUserStatusCB.setValue(userStatus.get(0));
 
+    }
+
+
+    private void checkPort(int port){
+        while(!isPortAviable(port))
+            System.out.println(">Порт "+(port++)+" занят");
+        serverPortTF.setText(Integer.toString(port));
+    }
+    private static boolean isPortAviable(int port) throws IllegalStateException {
+        try (Socket ignored = new Socket("localhost", port)) {
+            return false;
+        } catch (ConnectException e) {
+            return true;
+        } catch (IOException e) {
+            throw new IllegalStateException("Error while trying to check open port", e);
+        }
     }
 }
