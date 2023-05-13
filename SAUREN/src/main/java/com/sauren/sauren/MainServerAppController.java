876d20f712454b624974403180d581d81ac16a11
ilyasidor <deadboost.666@gmail.com>
2023-05-13 16:38:12 +0900

    Заполнение ChoiceBox значениями о статусе пользователя.
     ObservableList<String> userStatus = FXCollections.observableArrayList("Online", "Offline", "All Users");
            choiceUserStatusCB.setItems(userStatus);
            choiceUserStatusCB.setValue(userStatus.get(0));

--- SAUREN/src/main/java/com/sauren/sauren/MainServerAppController.java
+++ SAUREN/src/main/java/com/sauren/sauren/MainServerAppController.java
@@ -1,16 +1,23 @@
 package com.sauren.sauren;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.ObservableListBase;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 
 import java.net.Inet4Address;
 import java.net.URL;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class MainServerAppController implements Initializable
 {
+    @FXML
+    public ChoiceBox choiceUserStatusCB;
     @FXML
     private Label serverIPLbl;
     @Override
@@ -21,6 +28,11 @@ public class MainServerAppController implements Initializable
         } catch (UnknownHostException e) {
             throw new RuntimeException(e);
         }
+
+        //Заполнение ChoiceBox значениями о статусе пользователя.
+        ObservableList<String> userStatus = FXCollections.observableArrayList("Online", "Offline", "All Users");
+        choiceUserStatusCB.setItems(userStatus);
+        choiceUserStatusCB.setValue(userStatus.get(0));
 
     }
 }
