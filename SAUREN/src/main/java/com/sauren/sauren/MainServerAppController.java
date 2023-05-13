7307465edb6cc37a9db4aa2c9a0c020c48406172
Vlad <vladislavbymain@mail.ru>
2023-05-13 16:07:39 +0900

    added Initialize to controller

--- SAUREN/src/main/java/com/sauren/sauren/MainServerAppController.java
+++ SAUREN/src/main/java/com/sauren/sauren/MainServerAppController.java
@@ -1,5 +1,14 @@
 package com.sauren.sauren;
 
-public class MainServerAppController
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainServerAppController implements Initializable
 {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
