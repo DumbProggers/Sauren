3375d0f69be38f5b162780d8dd501dc97f800216
Vlad <vladislavbymain@mail.ru>
2023-05-13 16:28:12 +0900

    show current ip

--- SAUREN/src/main/java/com/sauren/sauren/MainServerAppController.java
+++ SAUREN/src/main/java/com/sauren/sauren/MainServerAppController.java
@@ -1,14 +1,26 @@
 package com.sauren.sauren;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
+import java.net.Inet4Address;
 import java.net.URL;
+import java.net.UnknownHostException;
 import java.util.ResourceBundle;
 
 public class MainServerAppController implements Initializable
 {
+    @FXML
+    private Label serverIPLbl;
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+        try {
+            serverIPLbl.setText(Inet4Address.getLocalHost().getHostAddress());
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
