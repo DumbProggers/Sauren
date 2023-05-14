a486f4dbb13cc88412a4f3a1567ae5e92d45dfe7
ilyasidor <deadboost.666@gmail.com>
2023-05-14 20:09:10 +0900

    добавил округление аватаров

--- SAUREN/src/main/java/com/sauren/sauren/UIelements/UserButton.java
+++ SAUREN/src/main/java/com/sauren/sauren/UIelements/UserButton.java
@@ -2,12 +2,21 @@ package com.sauren.sauren.UIelements;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
 
 public class UserButton extends VBox
 {
+    @FXML
+    private ImageView userIcoImg;
     @FXML
     private Label userNameLbl;
     @FXML
@@ -22,6 +31,14 @@ public class UserButton extends VBox
         try {
             loader.load();
         }catch(Exception ex){ex.printStackTrace();}
+
+        roundImage(userIcoImg);
+
+        // set a clip to apply rounded border to the original image.
+
+
+
+
     }
 
     public UserButton(String Name,String ip)
@@ -37,6 +54,29 @@ public class UserButton extends VBox
         userIPLbl.setText(ip);
     }
 
+
+    public static void roundImage(ImageView image){
+        Rectangle clip = new Rectangle(
+                image.getFitWidth(), image.getFitHeight()
+        );
+        clip.setArcWidth(100);
+        clip.setArcHeight(100);
+        image.setClip(clip);
+
+        // snapshot the rounded image.
+        SnapshotParameters parameters = new SnapshotParameters();
+        parameters.setFill(Color.TRANSPARENT);
+        WritableImage imageCh = image.snapshot(parameters, null);
+
+        // remove the rounding clip so that our effect can show through.
+        image.setClip(null);
+
+        // apply a shadow effect.
+        image.setEffect(new DropShadow(5, Color.GRAY));
+
+        // store the rounded image in the imageView.
+        image.setImage(imageCh);
+    }
     public void setUserName(String name)    {userNameLbl.setText(name);}
     public void setUserIp(String ip)    {userIPLbl.setText(ip);}
     public void setUserState(boolean online)
